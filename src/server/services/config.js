34b936ec83e5a5c476f29e27646add1c67803578
danraygentry1 <60275076+danraygentry1@users.noreply.github.com>
2022-11-12 17:03:09 -0700

    Update config.js
    pp fix

--- src/server/services/config.js
+++ src/server/services/config.js
@@ -6,8 +6,8 @@
       "host" : "api.paypal.com",
       "mode" : "live",
       "port" : "",
-      "client_id" : "AV5MkMmK1rO35qp0m4DLMLUJ1EM8E6GCZuhg3xET38zGcFplEHJVhNbBZxTknkW07PXQ83_Ktliu0I6J",  // your paypal serverlication client id
-      "client_secret" : "EH9MIbjtCjcBsjGc1Su-jDrVKjIdoaJUNTcHjG39ORwMi7lv1dSHYXR6xGsHX2CJXsDGT5j7aFz9onis" // your paypal serverlication secret id
+      "client_id" : "",  // your paypal serverlication client id
+      "client_secret" : "" // your paypal serverlication secret id
     }
     paypal.configure(config);
   };
